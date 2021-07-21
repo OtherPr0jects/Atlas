a5cba50011f90ed756526473e5ce44e9209071ea
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2021-07-21 13:09:31 -0400

    Updated.

--- Atlas/Addresses.h
+++ Atlas/Addresses.h
@@ -5,6 +5,6 @@ struct Addresses {
 	// Contains offset of address from base address.
 
 	// VFTables.
-	static const std::uintptr_t ScriptContext = 0x1D737A8; // Using ScriptContext to get the DataModel object because sometimes scanning for the DataModel VFTable leads to multiple results.
-	static const std::uintptr_t VisualEngine = 0x1F43B98;
+	static const std::uintptr_t ScriptContext = 0x1d86798; // Using ScriptContext to get the DataModel object because sometimes scanning for the DataModel VFTable leads to multiple results.
+	static const std::uintptr_t VisualEngine = 0x24bf0d8;
 };
